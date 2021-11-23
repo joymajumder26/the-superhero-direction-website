d42a7927e784748a14f57e7687068726bb6cb9f0
joymajumder26 <joymajumder34@gmail.com>
2021-11-23 12:15:57 +0600

    change

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <div className="header">
             <img src="https://www.bdtask.com/assets/images/bdtask-logo.png" alt="" />
-            <h2>Bdtask A Leading Software Company</h2>
+            <h2>Bdtask A Leading Software Company.</h2>
             <p>Bdtask Limited is one of the best and leading innovative software developing company in Bangladesh. </p>
         </div>
     );
